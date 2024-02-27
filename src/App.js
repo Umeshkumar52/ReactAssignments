2f95434130b56a7246dd9ca7a5700ccd0e253f41
Umeshkumar52 <uk3993907@gmail.com>
2024-02-27 15:43:27 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import {BrowserRouter,Route,Routes } from "react-router-dom";
-import Tailwind from "./pages/TailwindCss";
-
+import PwSkillsblog from "./pages/pwSkillsBlog/PwSkillsBlog";
+import ReadBlog from "./pages/pwSkillsBlog/ReadBlog";
+import Todo from '../src/pages/todoApp/Todo'
+import MoviSearch from '../src/pages/moviSearchApp/MoviSearch'
 function App() {
   return (
     <BrowserRouter>
        <Routes>
-         <Route path='/' element={<Tailwind/>}/>
+
+         <Route path='/' element={<PwSkillsblog/>}/>
+         <Route path='/readBlog' element={<ReadBlog/>}/>
+         <Route path='/todo' element={<Todo/>}/>
+         <Route path='/movi' element={<MoviSearch/>}/>
       </Routes>
    </BrowserRouter>
   );
